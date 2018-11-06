bf793d64ce736e75d4c9926c65c5dc97afb4967d
fahmisodret <fahmisodret@gmail.com>
2018-11-06 15:30:15 +0700

    add migration config for greenplaza config.

---
+++ database/migrations/2018_11_06_060900_create_conf_configs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConfConfigsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conf_configs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('configs_id');
+            $table->string('configs_name');
+            $table->text('configs_value');
+            $table->tinyInteger('configs_status');
+            $table->text('configs_note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conf_configs');
+    }
+}
