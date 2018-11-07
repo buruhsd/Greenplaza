fa22282d7941b1aa109115dac9245dc3cb08b768
wahyu-atmojo <miripandrewgarfield@gmail.com>
2018-11-07 23:11:07 +0700

    view index

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -9,130 +9,22 @@
         <![endif]-->
     
     @include('layouts.top-header')
-    @include('frontend.plugin.banner-4')
-    <!-- .shop-page-area start -->
-    <div class="shop-single-area">
+    <div class="breadcumb-area">
         <div class="container">
-            <div class="row revarce-wrap">
-                
-                <div class="col-9 col-lg-9 col-12">
-                    <div class="shop-area">
-
-                        @include('layouts.content-1')
+            <div class="row">
+            <div class="col-12">
+                    <div class="breadcumb-wrap bg-1">
+                        
                     </div>
-                </div>
-                <div class="col-lg-3 col-sm-6 col-12">
-                    <aside class="sidebar-area bg-1">
-                        <div class="widget widget_search">
-                            <h2 class="section-title">Search Product</h2>
-                            <form action="#" class="searchform">
-                                <input type="text" name="s" placeholder="Search Product...">
-                                <button type="submit"><i class="fa fa-search"></i></button>
-                            </form>
-                        </div>
-                        <div class="widget widget_categories">
-                            <h2 class="section-title">Categories</h2>
-                            <ul>
-                                <li><a href="#">Furniture</a></li>
-                                <li><a href="#">Chair & Table</a></li>
-                                <li><a href="#">Comfortable Sofa</a></li>
-                                <li><a href="#">Accessories</a></li>
-                                <li><a href="#">House Decoration</a></li>
-                                <li><a href="#">Kitchen</a></li>
-                            </ul>
-                        </div>
-                        <div class="product-sidebar">
-                            <h2 class="section-title">Related Product</h2>
-                            <div class="slidebar-product-wrap">
-                                <div class="product-sidebar-items fix">
-                                    <div class="product-sidebar-img black-opacity">
-                                        <img src="assets/images/product/sidebar/24.jpg" alt="">
-                                    </div>
-                                    <div class="product-sedebar-content fix">
-                                        <h4><a href="shop.html">Floral Print Buttoned</a></h4>
-                                        <ul class="rating">
-                                            <li><i class="fa fa-star"></i></li>
-                                            <li><i class="fa fa-star"></i></li>
-                                            <li><i class="fa fa-star"></i></li>
-                                            <li><i class="fa fa-star"></i></li>
-                                            <li><i class="fa fa-star-o"></i></li>
-                                        </ul>
-                                        <p>$20.00</p>
-                                    </div>
-                                </div>
-                                <div class="product-sidebar-items fix">
-                                    <div class="product-sidebar-img black-opacity">
-                                        <img src="assets/images/product/sidebar/23.jpg" alt="">
-                                    </div>
-                                    <div class="product-sedebar-content fix">
-                                        <h4><a href="shop.html">Push It Messenger Bag</a></h4>
-                                        <ul class="rating">
-                                            <li><i class="fa fa-star"></i></li>
-                                            <li><i class="fa fa-star"></i></li>
-                                            <li><i class="fa fa-star"></i></li>
-                                            <li><i class="fa fa-star"></i></li>
-                                            <li><i class="fa fa-star-o"></i></li>
-                                        </ul>
-                                        <p>$20.00</p>
-                                    </div>
-                                </div>
-                                <div class="product-sidebar-items fix">
-                                    <div class="product-sidebar-img black-opacity">
-                                        <img src="assets/images/product/sidebar/22.jpg" alt="">
-                                    </div>
-                                    <div class="product-sedebar-content fix">
-                                        <h4><a href="shop.html">Sprite Foam Yoga Brick</a></h4>
-                                        <ul class="rating">
-                                            <li><i class="fa fa-star"></i></li>
-                                            <li><i class="fa fa-star"></i></li>
-                                            <li><i class="fa fa-star"></i></li>
-                                            <li><i class="fa fa-star"></i></li>
-                                            <li><i class="fa fa-star-o"></i></li>
-                                        </ul>
-                                        <p>$20.00</p>
-                                    </div>
-                                </div>
-                                <div class="product-sidebar-items fix mb-0">
-                                    <div class="product-sidebar-img black-opacity">
-                                        <img src="assets/images/product/sidebar/21.jpg" alt="">
-                                    </div>
-                                    <div class="product-sedebar-content fix">
-                                        <h4><a href="shop.html">Dual Handle Cardio Ball</a></h4>
-                                        <ul class="rating">
-                                            <li><i class="fa fa-star"></i></li>
-                                            <li><i class="fa fa-star"></i></li>
-                                            <li><i class="fa fa-star"></i></li>
-                                            <li><i class="fa fa-star"></i></li>
-                                            <li><i class="fa fa-star-o"></i></li>
-                                        </ul>
-                                        <p>$20.00</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="tag-wrap">
-                            <h2 class="section-title">Propular Tags</h2>
-                            <ul>
-                                <li><a href="#">ecommerce</a></li>
-                                <li><a href="#">product</a></li>
-                                <li><a href="#">man</a></li>
-                                <li><a href="#">fan</a></li>
-                                <li><a href="#">woman</a></li>
-                                <li><a href="#">kids</a></li>
-                                <li><a href="#">babys</a></li>
-                                <li><a href="#">pant</a></li>
-                                <li><a href="#">kids</a></li>
-                                <li><a href="#">babys</a></li>
-                                <li><a href="#">pant</a></li>
-                                <li><a href="#">chair</a></li>
-                                <li><a href="#">table</a></li>
-                            </ul>
-                        </div>
-                    </aside>
-                </div>
+            </div>
             </div>
         </div>
     </div>
+    <!-- .shop-page-area start -->
+    
+                        <!-- @include('layouts.content-1') -->
+                        @yield('content')
+                   
     <!-- .shop-page-area enc -->
     <!-- footer -->
     @include('frontend.plugin.footer')
