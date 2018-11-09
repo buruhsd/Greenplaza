4ce92cd0cdd02393fd7e406abcefa58d20c11adf
fahmisodret <fahmisodret@gmail.com>
2018-11-09 13:37:05 +0700

    add function plugin

---
+++ app/Libraries/Plugin.php
@@ -0,0 +1,69 @@
+<?php
+class Plugin
+{
+
+    /**
+    * @param
+    * @return
+    */
+    public static function view($function = 'banner1', $param = []){
+        return Plugin::$function($param);
+    }
+
+    /**
+    * @param
+    * @return
+    */
+    public static function banner1(){
+        return view('frontend.plugin.banner-1');
+    }
+
+    /**
+    * @param
+    * @return
+    */
+    public static function banner2(){
+        return view('frontend.plugin.banner-2');
+    }
+
+    /**
+    * @param
+    * @return
+    */
+    public static function banner3(){
+        return view('frontend.plugin.banner-3');
+    }
+
+    /**
+    * @param
+    * @return
+    */
+    public static function banner4(){
+        return view('frontend.plugin.banner-4');
+    }
+
+    /**
+    * @param
+    * @return
+    */
+    public static function footer(){
+        return view('frontend.plugin.footer');
+    }
+
+    /**
+    * @param
+    * @return
+    */
+    public static function slider(){
+        return view('frontend.plugin.slider');
+    }
+
+    /**
+    * @param
+    * @return
+    */
+    public static function top_header(){
+        return view('frontend.plugin.top-header');
+    }
+
+}
