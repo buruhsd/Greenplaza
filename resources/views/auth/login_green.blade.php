399cf35d3d65957e1d66f6c3b3743b6a13275c8f
wahyu-atmojo <miripandrewgarfield@gmail.com>
2018-11-13 13:43:35 +0700

    view login

--- resources/views/auth/login_green.blade.php
+++ resources/views/auth/login_green.blade.php
@@ -12,23 +12,22 @@
 
 <form action="/action_page.php">
   <div class="container">
-    <a href="index.html">
-        <img class="dark-logo" src="{{ asset('frontend/images/logo-fix.png') }}" alt="" width="300px" height="100px">
+    <a class="gp" href="index.html" >
+        <img class="dark-logo" src="{{ asset('frontend/images/logo-fix.png') }}" alt="" width="100px" height="50px" >
     </a>
-    <h1>Login</h1>
-    <p>Please fill in this form to create an account.</p>
-    <hr>
+    <h2>Login</h3>
+    <p>Belum Punya Akun ? <a href="#"> Daftar disini </a>.</p>
 
-    <label for="email"><b>Sponsor</b></label>
+    <label for="email"><b>Email</b></label>
     <input type="text" placeholder="Enter Email" name="email" required>
 
     <label for="psw"><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="psw" required>
 
     <hr>
-    <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
 
     <button type="submit" class="registerbtn">Login</button>
+    <p><a href="#">Lupa Password</a>.</p>
   </div>
   
   <div class="container signin">
