b68c9ab3c36f1930e8bd2f8c1b62cb5dbda31850
fahmisodret <fahmisodret@gmail.com>
2018-11-14 12:51:59 +0700

    login

--- resources/views/auth/login_green.blade.php
+++ resources/views/auth/login_green.blade.php
@@ -16,15 +16,25 @@
         <a class="gp" href="index.html" >
             <img class="dark-logo" src="{{ asset('frontend/images/logo-fix.png') }}" alt="" width="200px" height="50px" >
         </a>
-        <p>Belum Punya Akun ? <a href="#"> Daftar disini </a>.</p>
+        <p>Belum Punya Akun ? <a href="{{ route('register') }}"> Daftar disini </a>.</p>
 
-{{--         <label for="email"><b>Email</b></label>
+        <label for="email"><b>Email</b></label>
         <input type="text" placeholder="Enter Email" name="email" required>
+        @if ($errors->has('email'))
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $errors->first('email') }}</strong>
+            </span>
+        @endif
 
         <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required> --}}
+        <input type="password" placeholder="Enter Password" name="password" required>
+        @if ($errors->has('password'))
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $errors->first('password') }}</strong>
+            </span>
+        @endif
 
-        <div class="form-group row">
+{{--         <div class="form-group row">
             <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
             <div class="col-md-6">
                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
@@ -46,7 +56,7 @@
                     </span>
                 @endif
             </div>
-        </div>
+        </div> --}}
         <hr>
 
         <button type="submit" class="registerbtn">Login</button>
