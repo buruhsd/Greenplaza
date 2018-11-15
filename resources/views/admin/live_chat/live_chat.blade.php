32b4acf4d7e44f7450f9b2fe58dbf3d7e9e89816
wahyu-atmojo <miripandrewgarfield@gmail.com>
2018-11-15 23:45:15 +0700

    live chat

---
+++ resources/views/admin/live_chat/live_chat.blade.php
@@ -0,0 +1,33 @@
+@extends('admin.index')
+@section('content')
+
+<div class="page-title">
+        <h3 class="breadcrumb-header">Live Chat</h3>
+            </div>
+                <form action="#" method="GET">
+                    <div class="panel-heading clearfix" style="margin-bottom: 10px;">
+                        <div class="input-group pull-left" style="width: 225px;">
+                            <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                            <input type="text" class="form-control" placeholder="Search" name="search" value="" autocomplete="off" id="search_table_currency">
+                        </div>
+                      </div> 
+                </form>
+                <div id="main-wrapper">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-white">
+                                <div class="panel-heading clearfix">
+                                    <h4 class="panel-title">Live Chat</h4>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="table-responsive">
+                                        Chat e belom bray
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- Row -->
+                </div>
+                
+@endsection
+        
