f0b831d3d9578012d9f8952ac7975b02c645ab89
wahyu-atmojo <miripandrewgarfield@gmail.com>
2018-11-15 23:45:06 +0700

    hot promo

---
+++ resources/views/admin/hot_promo/hot_promo.blade.php
@@ -0,0 +1,66 @@
+@extends('admin.index')
+@section('content')
+
+<div class="page-title">
+        <h3 class="breadcrumb-header">Hot Promo</h3>
+            </div>
+                <form action="#" method="GET">
+                    <div class="panel-heading clearfix" style="margin-bottom: 10px;">
+                        <div class="input-group pull-left" style="width: 225px;">
+                            <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                            <input type="text" class="form-control" placeholder="Search" name="search" value="" autocomplete="off" id="search_table_currency">
+                                </div>
+                                        </form>
+                                    <div class="input-group pull-left" style="width: 225px;">
+                                        <select class="form-control" id="sel1">
+                                            <option>Coba heuheu</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                        </select>
+                                    </div>
+                                        <div class="input-group pull-right" style="width: 225px;">
+                                        <button type="button" class="btn btn-default">Tambah Produk Baru</button>  
+                                        </div>              
+                                    </div> 
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    
+                                                    <div class="panel panel-white">
+                                                    
+                                                      
+                                                        <div class="panel-heading clearfix">
+                                                            <h4 class="panel-title">Hot Promo</h4>
+                                                        </div>
+                                                        <div class="panel-body">
+                                                            <div class="table-responsive">
+                                                                <table class="table">
+                                                                    <thead>
+                                                                        <tr>
+                                                                            <th>ID</th>
+                                                                            <th>Foto Produk</th>
+                                                                            <th>Detail Produk</th>
+                                                                            <th>Detail Seller</th>
+                                                                            <th>Status</th>
+                                                                            <th>Action</th>
+                                                                            
+                                                                        </tr>
+                                                                    </thead>
+                                                                    <tbody>
+                                                                        <tr>
+                                                                            <th scope="row"></th>
+                                                                            <td></td>
+                                                                            
+                                                                            
+                                                                        </tr>
+                                                                    </tbody>
+                                                                </table>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div><!-- Row -->
+                
+                
+@endsection
+        
