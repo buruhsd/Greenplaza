eb4f3c6af7d880e0898f39e0dc7e3ff2e8dab1bd
friska <friskasusanto8@gmail.com>
2018-11-29 12:04:12 +0700

    email

--- resources/views/admin/email-sender/email_sender.blade.php
+++ resources/views/admin/email-sender/email_sender.blade.php
@@ -2,30 +2,39 @@
 @section('content')
 
 <div class="page-title">
-        <h3 class="breadcrumb-header">Dashboard</h3>
+    <h3 class="breadcrumb-header">Dashboard</h3>
+</div>
+<div class="col-md-12">
+    <div class="panel-body">
+        <center><form>
+            <div class="form-group" style="width: 50%">
+                <input type="text" class="form-control" id="" placeholder="Input Email Sending">
             </div>
-                <form action="#" method="GET">
-                    <div class="panel-heading clearfix" style="margin-bottom: 10px;">
-                        <div class="input-group pull-left" style="width: 225px;">
-                            <span class="input-group-addon"><i class="fa fa-search"></i></span>
-                            <input type="text" class="form-control" placeholder="Search" name="search" value="" autocomplete="off" id="search_table_currency">
-                        </div>
-                      </div> 
-                </form>
-                <div id="main-wrapper">
-                    
-                                
-                                       <textarea class="form-control" id="article-ckeditor" name="description" placeholder="Post" rows="4"></textarea>
-                                   
-                            
-                </div>
-    <script src="{{ asset('/vendor/unisharp/laravel-ckeditor/ckeditor.js') }}"></script>
-    <script>
-        CKEDITOR.replace( 'article-ckeditor' , {
+            <textarea class="form-control" id="article-ckeditor" name="description" placeholder="Post" rows="6"></textarea>
+            <div class="col-md-6" style="margin-top: 3%">
+                <button type="submit" class="btn btn-primary">Send</button>
+            </div>
+            <div class="col-md-6" style="margin-top: 3%">
+                <button type="submit" class="btn btn-primary">Send for All</button>
+            </div>
+        </form></center>
+    </div>
+</div>
+    
+
+<script src="{{asset('backend/vendor/unisharp/laravel-ckeditor/ckeditor.js')}}"></script>
+<script>
+    CKEDITOR.replace( 'article-ckeditor', {
             height: 400,
             
-        });
-    </script>            
+        } );
+
+        function myFunction(x) {
+            var copyText = document.getElementById(x);
+            copyText.select();
+            document.execCommand("Copy");
+        } 
+</script>            
                 
 @endsection
         
