5a1628446a64be560acedb6850ca47ddf3b74c35
fahmisodret <fahmisodret@gmail.com>
2019-01-28 13:20:28 +0700

    pick address

--- resources/views/localapi/pick-address.blade.php
+++ resources/views/localapi/pick-address.blade.php
@@ -12,26 +12,31 @@
             <div class="table-responsive">
                 <table class="table m-t-20 respon">
                     <thead>
-                        <th>Name</th>
-                        <th>Phone</th>
-                        <th>Telpon</th>
-                        <th>Province</th>
-                        <th>City</th>
-                        <th>Subdistrict</th>
-                        <th>Postal Code</th>
-                        <th>Address</th>
-                        <th>Pick</th>
+                        <th></th>
+                        <th>Info</th>
+                        <th>Alamat</th>
+                        <th>Aksi</th>
                     </thead>
                     @foreach($user_address as $item)
                         <tbody>
-                            <td>{{$item->user_address_owner}}</td>
-                            <td>{{$item->user_address_phone}}</td>
-                            <td>{{$item->user_address_tlp}}</td>
-                            <td>{{$item->user_address_province}}</td>
-                            <td>{{$item->user_address_city}}</td>
-                            <td>{{$item->user_address_subdist}}</td>
-                            <td>{{$item->user_address_pos}}</td>
-                            <td>{{$item->user_address_address}}</td>
+                            <td></td>
+                            <td>
+                                <ul>
+                                    <li>Penerima : {{$item->user_address_owner}}</li>
+                                    <li>No. Hp : {{$item->user_address_phone}}</li>
+                                    <li>Telpon : {{$item->user_address_tlp}}</li>
+                                </ul>
+                            </td>
+                            <td>
+                                <ul>
+                                    <li>Alamat : {{FunctionLib::address_info($item->id)}}</li>
+                                    {{-- <li>Propinsi : {{$item->user_address_province}}</li>
+                                    <li>Kota / Kab : {{$item->user_address_city}}</li>
+                                    <li>Desa / Kelurahan : {{$item->user_address_subdist}}</li>
+                                    <li>Alamat : {{$item->user_address_address}}</li> --}}
+                                    <li>Kode Pos : {{$item->user_address_pos}}</li>
+                                </ul>
+                            </td>
                             <td>
                                 <input type="button" class="btn btn-success btn-sm" onclick="$(this).closest('.modal').modal('hide');use_address({{$item->id}}, '{{$item->user_address_label}}', {{$item->user_address_city}}, {{$item->user_address_subdist}});" value="Use {{$item->user_address_label}}">
                             </td>
