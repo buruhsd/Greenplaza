24aabc7212881cff51c7fe177ff2894dd578b76d
fahmisodret <fahmisodret@gmail.com>
2019-01-30 02:13:32 +0700

    env tidak terbaca

--- app/Http/Controllers/LocalApi/MidtransController.php
+++ app/Http/Controllers/LocalApi/MidtransController.php
@@ -117,7 +117,6 @@ class MidtransController extends Controller
     * @return
     */
     public function payment($param=[]){
-        dd(env('VERYTRANS_KEY'));
         if(Session::has('chart') && FunctionLib::array_sum_key(Session::get('chart'), 'trans_detail_amount_total') > 0){
             $data = Session::get('chart');
             $trans = [];
