c33a83ad8ba71c40062ab2197ed583bb8473a72f
fahmisodret <fahmisodret@gmail.com>
2019-01-30 01:32:01 +0700

    tes payment

--- app/Http/Controllers/LocalApi/MidtransController.php
+++ app/Http/Controllers/LocalApi/MidtransController.php
@@ -117,6 +117,7 @@ class MidtransController extends Controller
     * @return
     */
     public function payment($param=[]){
+        dd($param);
         if(Session::has('chart') && FunctionLib::array_sum_key(Session::get('chart'), 'trans_detail_amount_total') > 0){
             $data = Session::get('chart');
             $trans = [];
