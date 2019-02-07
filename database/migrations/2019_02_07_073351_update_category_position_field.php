91d811fed4742d4737ac5187060ae52295b1433b
friska <friskasusanto8@gmail.com>
2019-02-07 14:37:19 +0700

    migration

---
+++ database/migrations/2019_02_07_073351_update_category_position_field.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateCategoryPositionField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sys_category', function ($table) {
+            $table->integer('position')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sys_category', function ($table) {
+            $table->string('position');
+        });
+    }
+}
