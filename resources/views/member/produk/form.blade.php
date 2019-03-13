e12575a0c27086fb02de93eec10bbe9a41db07e5
friska <friskasusanto8@gmail.com>
2019-03-13 15:45:34 +0700

    hiden produk green member

--- resources/views/member/produk/form.blade.php
+++ resources/views/member/produk/form.blade.php
@@ -136,8 +136,11 @@
             {!! Form::label('produk_category_id', 'Kategori : ', ['class' => 'col-md-3 control-label']) !!}
             <div class="col-md-9">
                 <select name='produk_category_id' class="form-control">
+                    <option value=''>-- Pilih Kategori --</option>
                     @foreach($category as $item)
+                    @if ($item->category_name != 'Green Productions')
                     <option value='{{$item->id}}'>{{ucfirst(strtolower($item->category_name))}}</option>
+                    @endif
                     @endforeach
                 </select>
                 {!! $errors->first('produk_category_id', '<p class="help-block">:message</p>') !!}
@@ -148,8 +151,11 @@
             {!! Form::label('produk_category_id', 'Kategori : ', ['class' => 'col-md-3 control-label']) !!}
             <div class="col-md-9">
                 <select name='produk_category_id' class="form-control">
+                    <option value=''>-- Pilih Kategori --</option>
                     @foreach($category as $item)
+                    @if ($item->category_name != 'Green Productions')
                         <option value='{{$item->id}}' <?php if($produk->produk_category_id == $item->id){echo "selected";}?> >{{ucfirst(strtolower($item->category_name))}}</option>
+                    @endif
                     @endforeach
                 </select>
                 {!! $errors->first('produk_category_id', '<p class="help-block">:message</p>') !!}
