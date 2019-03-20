12a4e34c225f369307f6dea5b333ae054a54b3fc
fahmisodret <fahmisodret@gmail.com>
2019-03-21 01:23:22 +0700

    pusher

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -38,7 +38,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot ()
     {
-        // URL::forceScheme('https');
+        URL::forceScheme('https');
         Schema::defaultStringLength(191);
         // ketika ada aktifitas wallet
         Wallet::observe(WalletLogObserver::class);
