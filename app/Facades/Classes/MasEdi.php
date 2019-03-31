178b53352a667b19d4f9c9ce7c4b8ce87e3efeb8
wahyu-atmojo <miripandrewgarfield@gmail.com>
2019-03-31 22:35:01 +0700

    api server test mas edi

--- app/Facades/Classes/MasEdi.php
+++ app/Facades/Classes/MasEdi.php
@@ -12,7 +12,8 @@ class MasEdi extends Controller
 {
     // private $__api_server='http://wallet.cavallocoin.io/api'; //api
     private $__api_key = "c6cd80e53a26df51bad3773ef6f76331";
-    private $__api_server = 'http://45.76.176.231/edisedis/index.php';
+    private $__api_server = 'greenplaza.harmonyb12.com/edisedis/index.php';
+    // private $__api_server = 'http://45.76.176.231/edisedis/index.php';
     private $__api_username='fahmisodret-778181514'; //api key
 
     /**
