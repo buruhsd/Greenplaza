3d4a31629edf6acb5571b5eb1801bdab348926b9
fahmisodret <fahmisodret@gmail.com>
2019-04-22 13:51:52 +0700

    live chat

--- resources/views/admin/live_chat/live_chat.blade.php
+++ resources/views/admin/live_chat/live_chat.blade.php
@@ -17,7 +17,7 @@
                         <div class="col-md-12">
                             <div class="panel panel-white">
                                 <div class="panel-body">
-                                    <b>username : {{FunctionLib::get_config('username_tawk')}}, Password : {{FunctionLib::get_config('password_tawk')}}</span>
+                                    <b>username : {{FunctionLib::get_config('profil_username_tawk')}}, Password : {{FunctionLib::get_config('profil_password_tawk')}}</span>
                                     <iframe name="main" style="display: block;background: #000;border: none;height: 100vh;width: 100%;" id="main" src="https://dashboard.tawk.to" frameborder="0" align="left"><FONT FACE=ARIAL SIZE=3 COLOR="RED">Your Browser doesn't Support Required Component.</FONT></iframe>
                                 </div>
                             </div>
