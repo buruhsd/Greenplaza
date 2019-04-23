b491050c489fb7afdc99c2bfb197770065a02d33
fahmisodret <fahmisodret@gmail.com>
2019-04-23 11:03:56 +0700

    live chat show

--- resources/views/admin/live_chat/live_chat.blade.php
+++ resources/views/admin/live_chat/live_chat.blade.php
@@ -4,14 +4,6 @@
 <div class="page-title">
         <h3 class="breadcrumb-header">Live Chat</h3>
             </div>
-                <form action="#" method="GET">
-                    <div class="panel-heading clearfix" style="margin-bottom: 10px;">
-                        <div class="input-group pull-left" style="width: 225px;">
-                            <span class="input-group-addon"><i class="fa fa-search"></i></span>
-                            <input type="text" class="form-control" placeholder="Search" name="search" value="" autocomplete="off" id="search_table_currency">
-                        </div>
-                      </div> 
-                </form>
                 <div id="main-wrapper">
                     <div class="row">
                         <div class="col-md-12">
