c909308e9667c6388fc392f39bf35ed3cb80fed7
fahmisodret <fahmisodret@gmail.com>
2019-05-07 12:58:54 +0700

    tes

--- app/Http/Controllers/Member/HomeController.php
+++ app/Http/Controllers/Member/HomeController.php
@@ -100,7 +100,7 @@ class HomeController extends Controller
                             // t = document.getElementsByTagName('script')[0];
                             // t.parentNode.insertBefore(s, t);
                         }
-                        var tes = loadComponent(<?php route('localapi.masedi.payment');?>, 'a');
+                        var tes = loadComponent("<?php route('localapi.masedi.payment');?>", 'a');
                         console.log(tes);
                     </script>
                 <?php
