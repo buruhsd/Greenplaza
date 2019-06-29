01ed8dccc6ecf5a946b084b196d94994c915bbde
fahmisodret <fahmisodret@gmail.com>
2019-06-29 16:48:22 +0700

    bug

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -1140,7 +1140,7 @@ class ApiController extends Controller
     * mendapatkan detail produk
     **/
     public function detail_produk(Request $request, $pid){
-        $param = $request->input("username");
+        $param = $request->all()->username;
         return response()->json(['status' => 500, 'data'=>$param]);
         $asset = asset('assets/images/product/thumb');
         $data = Produk::where('sys_produk.id', $pid)
