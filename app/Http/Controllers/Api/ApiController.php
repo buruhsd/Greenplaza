c2e29599853f8e8f49bbae765b520a4ac23e70a5
fahmisodret <fahmisodret@gmail.com>
2019-07-11 12:30:20 +0700

    bug

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -408,12 +408,12 @@ class ApiController extends Controller
     public function payment_saldo(Request $request){
         $status = 500;
         $param = json_decode($request->getContent(), true);
-        $auth = [
-            'username' => $param["username"],
-            'password' => $param["password"],
-        ];
-        $check = FunctionLib::check_api_auth($auth);
-        return response()->json($param["cart"]);
+        // $auth = [
+        //     'username' => $param["username"],
+        //     'password' => $param["password"],
+        // ];
+        // $check = FunctionLib::check_api_auth($auth);
+        return response()->json($param);
         // if($check == 200){            
         //     if(Session::has('chart') && FunctionLib::array_sum_key(Session::get('chart'), 'trans_detail_amount_total') > 0){
         //         $data = Session::get('chart');
