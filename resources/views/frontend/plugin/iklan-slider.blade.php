67c47cce3b6bddbb8ff0d22c3f918046a9289f56
Atmojo <miripandrewgarfield@gmail.com>
2020-02-21 11:43:17 +0700

    image

--- resources/views/frontend/plugin/iklan-slider.blade.php
+++ resources/views/frontend/plugin/iklan-slider.blade.php
@@ -1,11 +1,63 @@
-@foreach($iklan as $item)
+{{-- @foreach($iklan as $item) --}}
     <div class="slider-item black-opacity">
-        @if($item->iklan_link == null || $item->iklan_link == '')
+        {{-- @if($item->iklan_link == null || $item->iklan_link == '')
             <a href="{{route('etalase', 'green-production')}}">
         @else
             <a href="{{$item->iklan_link}}">
         @endif
         <img src="{{asset('assets/images/iklan/'.$item->iklan_image)}}" style="height: 
+            310px"> --}}
+        <img src="{{asset('gi_image/iklan/poster1.png')}}" style="height: 
+            310px">
+        {{-- <img src="{{asset('gi_image/iklan/poster2.png')}}" style="height: 
+            310px">
+        <img src="{{asset('gi_image/iklan/poster3.png')}}" style="height: 
+            310px"> --}}
+        <div class="slider-content text-right">
+            <!-- <h2>Shop Our <span> DrakShop</span></h2>
+            <h3><span>35% </span> Discount</h3>
+            <ul>
+                <li><a href="shop.html">Comming Soon</a></li>
+            </ul> -->
+        </div>
+        </a>
+    </div>
+    <div class="slider-item black-opacity">
+        {{-- @if($item->iklan_link == null || $item->iklan_link == '')
+            <a href="{{route('etalase', 'green-production')}}">
+        @else
+            <a href="{{$item->iklan_link}}">
+        @endif
+        <img src="{{asset('assets/images/iklan/'.$item->iklan_image)}}" style="height: 
+            310px"> --}}
+       {{--  <img src="{{asset('gi_image/iklan/poster1.png')}}" style="height: 
+            310px"> --}}
+        <img src="{{asset('gi_image/iklan/poster2.png')}}" style="height: 
+            310px">
+        {{-- <img src="{{asset('gi_image/iklan/poster3.png')}}" style="height: 
+            310px"> --}}
+        <div class="slider-content text-right">
+            <!-- <h2>Shop Our <span> DrakShop</span></h2>
+            <h3><span>35% </span> Discount</h3>
+            <ul>
+                <li><a href="shop.html">Comming Soon</a></li>
+            </ul> -->
+        </div>
+        </a>
+    </div>
+    <div class="slider-item black-opacity">
+        {{-- @if($item->iklan_link == null || $item->iklan_link == '')
+            <a href="{{route('etalase', 'green-production')}}">
+        @else
+            <a href="{{$item->iklan_link}}">
+        @endif
+        <img src="{{asset('assets/images/iklan/'.$item->iklan_image)}}" style="height: 
+            310px"> --}}
+       {{--  <img src="{{asset('gi_image/iklan/poster1.png')}}" style="height: 
+            310px"> --}}
+        {{-- <img src="{{asset('gi_image/iklan/poster2.png')}}" style="height: 
+            310px"> --}}
+        <img src="{{asset('gi_image/iklan/poster3.png')}}" style="height: 
             310px">
         <div class="slider-content text-right">
             <!-- <h2>Shop Our <span> DrakShop</span></h2>
@@ -16,4 +68,4 @@
         </div>
         </a>
     </div>
-@endforeach
+{{-- @endforeach --}}
