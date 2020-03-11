4d3bdf1c9774f7f07f11d81de326afe06298b39e
fahmisodret <fahmisodret@gmail.com>
2020-03-12 05:05:44 +0700

    api

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -27,8 +27,7 @@ class ApiController extends Controller
     **/
     public function konfirmasi(Request $request, $id)
     {
-        return $id;
-        $where .= 'sys_trans.id ='.$id;
+        $where .= 'trans_detail_trans_id ='.$id;
         // status transaksi
         $w_status = ' AND trans_detail_status = 1 AND trans_detail_is_cancel != 1'; 
         $where .= $w_status;
