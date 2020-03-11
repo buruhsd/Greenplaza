809503b6978a17f3464ad83dd2835d1cc25dfd4e
fahmisodret <fahmisodret@gmail.com>
2020-03-12 02:02:32 +0700

    apiController

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -42,8 +42,7 @@ class ApiController extends Controller
                     }
                 }
                 // add to DB sys_trans
-                return User::where('username', $request->get('id'))->first();
-                $bank_id = User::where('username', $request->get('id'))->first()->user_bank()->where('user_bank_status', 1)->first()['id'];
+                $bank_id = User::find($request->get('id'))->user_bank()->where('user_bank_status', 1)->first()['id'];
                 if(!$bank_id || empty($bank_id) || $bank_id == null){
                     $status = 500;
                     $message = 'Silahkan isikan data bank anda.';
