414420e5966999189348c2f9446b441c44916c0f
wahyu-atmojo <miripandrewgarfield@gmail.com>
2020-08-13 11:32:27 +0700

    chart api

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -1950,4 +1950,72 @@ class ApiController extends Controller
             $user = Wallet::where('wallet_type', 7)->with(['user', 'type'])->get();
             return response()->json(['status' => 200, 'data' =>$user]);
     }
+
+    public function addChart(Request $request){
+        $kurs = json_decode(FunctionLib::cekKurs(), true);
+        $gln = json_decode(FunctionLib::priceGln(), true);
+        $gln2 = $gln['price'];
+
+        $id = $request->id_produk;
+        $produk = Produk::where('id', $id)->first();
+        if($request->qty > $produk['produk_stock'] || $request->qty <= 0 || $request->qty == null || $request->qty == ""){
+            return redirect()->back()->with(['flash_status' => 500,'flash_message' => 'Stock barang tidak mencukupi.']);
+        }
+        if($request->address_id == null || $request->address_id == ""){
+            return redirect()->back()->with(['flash_status' => 500,'flash_message' => 'Silahkan isi alamat anda']);
+        }
+        if($request->ship_cost == null || $request->ship_cost == "" || $request->ship_cost == 0){
+            return redirect()->back()->with(['flash_status' => 500,'flash_message' => 'Silahkan isi jasa pengiriman']);
+        }
+        // random string
+        $trans_code = FunctionLib::str_rand(8);
+
+        $courier = 0;
+        // print_r($request->courier);
+        if(!empty($request->courier)){
+            $courier = Shipment::where('shipment_name', '=', strtoupper($request->courier))->pluck('id')[0];
+        }
+        if($courier == null || $courier == "" || $courier == 0){
+            return redirect()->back()->with(['flash_status' => 500,'flash_message' => 'Silahkan isi jasa pengiriman']);
+        }
+
+        $price = $produk['produk_price'] * $request->qty;
+        $price_myr = $produk['price_myr'] * $request->qty;
+        $price_gln = $produk['gln_coin'] * $request->qty;
+        $transaction = [
+            'trans_code' => $trans_code,
+            'trans_detail_produk_id' => $produk['id'],
+            'trans_detail_shipment_id' => $courier,
+            'trans_detail_shipment_service' => $request->ship_service,
+            'trans_detail_user_address_id' => intval($request->address_id),
+            'trans_detail_no_resi' => "",
+            'trans_detail_qty' => $request->qty,
+            'trans_detail_size' => $request->size,
+            'trans_detail_color' => $request->color,
+            'trans_detail_amount' => $price,
+            'trans_detail_amount_myr' => $price_myr,
+            'trans_detail_amount_gln' => $price_gln,
+            'trans_detail_amount_ship' => $request->ship_cost,
+            'trans_detail_amount_total' => ($price + $request->ship_cost),
+            'trans_detail_amount_total_myr' => ($price_myr + $request->ship_cost),
+            'trans_detail_amount_total_gln' => $price_gln + ($request->ship_cost/$gln2),
+            'trans_detail_status' => 0,
+            'trans_detail_note' => $request->note,
+            'myr' => $kurs['Data']['MYR']['Beli']
+        ];
+        $status = 200;
+        $message = 'Item pembelian berhasil masuk ke chart.';
+        return response()->json(['status' => 200, 'message'=> $message, 'data' => $transaction]);
+        // var_dump($transaction); die();
+        // dd($transaction);
+        // if(!Session::has('chart')){
+        //     Session::put('chart', []);
+        // }
+        // Session::push('chart', $transaction);
+        // Session::save();
+
+        // $status = 200;
+        // $message = 'Item pembelian berhasil masuk ke chart.';
+        // return response()->json(['status' => 200, 'message' =>$message]);
+    }
 }
