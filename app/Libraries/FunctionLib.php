04e44651628ffa9611afbf58d7a75e4216c49d94
wahyu-atmojo <miripandrewgarfield@gmail.com>
2020-08-25 10:55:58 +0700

    up

--- app/Libraries/FunctionLib.php
+++ app/Libraries/FunctionLib.php
@@ -40,7 +40,7 @@ class FunctionLib
         if ($err) {
             $status = 500;
             $message = 'curl error.';
-            $response = ['status'=>$status, 'message'=>'kurs error lo'];
+            $response = ['status'=>$status, 'message'=>$message];
             return $response;
         } else {
             return $response;
@@ -74,7 +74,7 @@ class FunctionLib
         if ($err) {
             $status = 500;
             $message = 'curl error.';
-            $response = ['status'=>$status, 'message'=>"error hahahahha"];
+            $response = ['status'=>$status, 'message'=>$message];
             return $response;
         } else {
             return $response;
